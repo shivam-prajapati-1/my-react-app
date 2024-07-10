6c5b2375143ce7a6c7cac97cd4caafb939123224
shivam-prajapati-1 <163870678+shivam-prajapati-1@users.noreply.github.com>
2024-07-10 19:34:32 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div> 
+      <h1>hdhsdhjd</h1>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export const Home = (news) => {
+    console.log();
+  return (
+    <div>
+         {/* <p>Lorem ipsum, dolor sit amet minima voluptatem rem deleniti neque?</p> */}
+
+         <h1>hom.b {news.b}</h1>
+        
+    </div>
+    
+   
+  )
+}
+
+export default Home
